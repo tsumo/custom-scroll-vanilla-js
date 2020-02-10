f163744a791f45d7b3e09e6083f0071de6fd6df3
tsumo <tsumo@users.noreply.github.com>
2020-02-10 18:10:04 +0300

    First version

---
+++ script.js
@@ -0,0 +1,65 @@
+"use strict";
+
+const content = document.querySelector(".content");
+
+const scroll = document.querySelector(".scroll");
+
+let contentHeight = 0;
+let windowHeight = 0;
+
+let scrollSizePercent = 0;
+let scrollSizePx = 0;
+
+let scrollPosPercent = 0;
+let scrollPosPx = 0;
+
+let scrollDragOffsetPx = 0;
+
+const moveScroll = n => {
+  const scrollOffset = Math.min(
+    windowHeight - scrollSizePx,
+    Math.max(0, n - scrollDragOffsetPx)
+  );
+  scrollPosPercent = scrollOffset / (windowHeight - scrollSizePx);
+  scroll.style.top = `${scrollOffset}px`;
+};
+
+const adjustContent = () => {
+  const contentOffset = scrollPosPercent * (contentHeight - windowHeight);
+  content.style.transform = `translateY(-${contentOffset}px)`;
+};
+
+const calculateScrollSize = () => {
+  contentHeight = content.getBoundingClientRect().height;
+  windowHeight = window.innerHeight;
+  scrollSizePercent = 1 / (contentHeight / windowHeight);
+  scrollSizePx = windowHeight * scrollSizePercent;
+  scroll.style.height = `${scrollSizePx}px`;
+  moveScroll(scrollPosPx);
+  adjustContent();
+};
+calculateScrollSize();
+
+window.addEventListener("resize", calculateScrollSize);
+
+const dragStart = e => {
+  // console.log("start drag");
+  scrollDragOffsetPx = e.layerY;
+  document.addEventListener("mousemove", dragging);
+  document.addEventListener("mouseup", dragEnd, { once: true });
+};
+
+const dragging = e => {
+  // console.log("dragging");
+  moveScroll(e.clientY);
+  adjustContent();
+  scrollPosPx = e.clientY;
+};
+
+const dragEnd = () => {
+  // console.log("end drag");
+  scrollDragOffsetPx = 0;
+  document.removeEventListener("mousemove", dragging);
+};
+
+scroll.addEventListener("mousedown", dragStart);
